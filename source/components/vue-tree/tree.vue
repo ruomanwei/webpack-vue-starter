ae68a5ab0e2c0fb62ea6214ed495c930a81ef6a7
陈天宇 <313477629@qq.com>
2017-11-10 20:21:48 +0800

    update babel‘s cfg to stage-2

--- source/components/vue-tree/tree.vue
+++ source/components/vue-tree/tree.vue
@@ -14,6 +14,18 @@ export default {
         return{
             aa:"Hello world"
         }
+    },
+    created(){
+        danielchan()
+    },
+    methods:{
+        danielchan(){
+            const aa = {a:1,b:2,c:3};
+            var cc = {...aa,cc:4}
+            let dd = `asdf${cc}`;
+            
+            return dd.repeat(4);
+        }
     }
 };
 </script>
